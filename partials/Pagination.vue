30361359ee6854454ff7450202f76c51a4ac6eec
Sam <sam@rat.md>
2026-08-29 11:12:33 +0200

    [FIX] minor changes

--- partials/Pagination.vue
+++ partials/Pagination.vue
@@ -55,6 +55,7 @@ export interface PaginationLink {
 
 export interface ComponentProps {
     current_page: number;
+    last_page: number;
     first_page_url: string | null;
     last_page_url: string | null;
     prev_page_url: string | null;
